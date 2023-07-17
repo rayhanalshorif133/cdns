1f4ab02251785a04295bd44609f5c397f6b1b881
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-17 16:03:09 +0600

    autopage home set

--- js/handleHomePageRedirection.js
+++ js/handleHomePageRedirection.js
@@ -1,5 +1,11 @@
 const handleHomePageRedirection = () => {
-    console.log('handleHomePageRedirection')
+    const currentLocation = window.location.href;
+    const pageIndex = currentLocation.indexOf('github');
+    if(pageIndex !== -1) {
+        $('.cdnHome').attr('href', '/cdns/');
+    }else{
+        $('.cdnHome').attr('href', '/');
+    }
 };
 
 export default handleHomePageRedirection;
